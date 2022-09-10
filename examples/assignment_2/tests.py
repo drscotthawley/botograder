ab53f5748979fd4c654edfc9a7928e81c3f65de8
Scott H. Hawley <scott.hawley@belmont.edu>
2022-09-09 21:48:32 -0500

    just cleaned up the tests a bit

--- examples/assignment_2/tests.py
+++ examples/assignment_2/tests.py
@@ -1,41 +1,15 @@
 
-def test_func(func_name):
-    "returns True/False 1/0 if test passes/fails"
-    try:
-        print(f"  --- Running test of {func_name.__name__[5:]}():")
-        func_name()
-        print("      Passed.")
-        return 1
-    except:
-        _, _, tb = sys.exc_info()
-        traceback.print_tb(tb) # Fixed format
-        tb_info = traceback.extract_tb(tb)
-        filename, line, func, text = tb_info[-1]
+# instructor-supplied tests. each takes no arguments, each one contains an assert
 
-        print(f'An error occurred when running {func_name} in statement: {text}')
-        return 0
-
-
-
-
-#  some tests 
-
-x2 = torch.tensor([[1,2,3],[4,5,6]])
-print("x2 =\n",x2)
-x2 = x2.float() # actually let's make it floats instead of ints
-print("x2 =\n",x2)
+x2 = torch.tensor([[1,2,3],[4,5,6]]).float()  # some inputs to use for the tests
 batch_size, in_dim = x2.shape[0], x2.shape[1]; 
-print(f"batch_size = {batch_size}, in_dim = {in_dim}")
 
 torch.manual_seed(0)
 mynet2 = MyNet( in_dim, 10, 10, 1 )
-
 p2 = mynet2.forward(x2).detach().numpy()
-print(p2)
-
-tol = 1e-6   # some tolerable level of precision
 target = np.array([[-0.13569488], [-0.18428665]])  # my answer (to some precision based on cut & paste!)
 
+tol = 1e-6   # some tolerable level of precision
 
 def test1():
     assert np.abs(p2 - target).sum() < tol, "Nope the difference is too great."
@@ -62,9 +36,31 @@ def test3():
 
 
 
-func_list = [test1, test2, test3]
+def test_func(func_name):
+    """
+    Generic function tester: returns True/False 1/0 if test passes/fails. 
+    Passes = executes w/o error, e.g. w/o failed asserts
+    """
+    try:
+        print(f"  --- Running test of {func_name.__name__[5:]}():")
+        func_name()
+        print("      Passed.")
+        return 1
+    except:
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb) # Fixed format
+        tb_info = traceback.extract_tb(tb)
+        filename, line, func, text = tb_info[-1]
+
+        print(f'An error occurred when running {func_name} in statement: {text}')
+        return 0
+
+
+
+
+func_list = [test1, test2, test3]  # 
 passed = 0
 for func in func_list:
-    passed += test_func(func)
+    passed += test_func(func)  # TODO: maybe not best to have to supply test_func for each assignment
 
 print(f"\n{passed}/{len(func_list)} tests passed.")
